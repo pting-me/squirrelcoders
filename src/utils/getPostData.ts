3e7b9a8690a19759254b3514eae4e2c5a5c35740
Phil Ting <pting.me+gh@gmail.com>
2022-11-07 12:41:06 -0800

    fixed typescript issues

--- src/utils/getPostData.ts
+++ src/utils/getPostData.ts
@@ -1,14 +1,16 @@
-import readingTime from 'reading-time'
+import readingTime from 'reading-time';
 
 type Post = {
-  title: string
-  file: string
-  rawContent: () => string
-}
+  title: string;
+  file: string;
+  rawContent: () => string;
+};
 
 export default function getPostData(post: Post) {
+  // matches filename (after last slash '/')
+  // drops extension (after '.' if it exists)
   return {
-    slug: post.file.split('/').pop().split('.').shift(),
+    slug: post.file.match(/([^/.]+)\.{0,1}[^.]+$/) ?? post.file,
     readingTime: readingTime(post.rawContent()).text,
-  }
+  };
 }
